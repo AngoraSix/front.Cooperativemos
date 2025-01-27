aae3f12ea3382fc471a874890b1f42b385bc9ff3
Ger Roza <rozagerardo@gmail.com>
2025-01-27 20:13:53 -0300

    Added support for Google Tag Manager

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import { Head, Html, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 import config from '../config';
 
 export default function Document() {
@@ -22,8 +23,21 @@ export default function Document() {
           content={head.image.logo}
         />
         <meta property="fb:app_id" key="fb.id" content={head.facebookAppId} />
+        {/* Google Tag Manager - Script (HEAD) */}
+        <Script id="gtm-script-head">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${config.thirdParties.googleTagManager.id}');`}
+        </Script>
       </Head>
       <body>
+        {/* Google Tag Manager - Script (BODY - noscript) */}
+        <noscript>
+          <iframe src={`https://www.googletagmanager.com/ns.html?id=${config.thirdParties.googleTagManager.id}`}
+            height="0" width="0" style="display:none;visibility:hidden"></iframe>
+        </noscript>
         <Main />
         <NextScript />
       </body>
