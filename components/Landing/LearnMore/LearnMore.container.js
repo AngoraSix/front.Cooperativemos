8ef9ad44a8f4aac2fa11ccdb999f9e370151b544
Ger Roza <rozagerardo@gmail.com>
2025-01-28 00:10:03 -0300

    Cleaned up code

--- components/Landing/LearnMore/LearnMore.container.js
+++ components/Landing/LearnMore/LearnMore.container.js
@@ -58,6 +58,9 @@ const LearnMoreContainer = ({
   const [activeStep, setActiveStep] = useState(0);
   const totalSteps = 2; // We’ll have 3 steps
 
+  console.log("GERGERGER");
+  console.log(config.thirdParties.googleRecaptcha.secret)
+
   // Basic form fields
   const [email, setEmail] = useState(session?.user?.email || '');
   const [role, setRole] = useState('');
