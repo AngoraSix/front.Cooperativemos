2875576be3b63f56a5ccbe1c370b0c5b29a63612
Ger Roza <rozagerardo@gmail.com>
2025-03-12 18:11:53 -0300

    Implemented section 4 Hook for both desktop and mobile

--- components/Landing/Landing/Landing.component.js
+++ components/Landing/Landing/Landing.component.js
@@ -3,8 +3,8 @@ import { useTranslation } from 'next-i18next';
 import React from 'react';
 import SectionAimedAt from './Sections/SectionAimedAt.component';
 import SectionBenefit from './Sections/SectionBenefit.component';
+import SectionHook from './Sections/SectionHook.component';
 import SectionIntro from './Sections/SectionIntro.component';
-
 
 const Lanading = ({ }) => {
   const { t } = useTranslation('landing');
@@ -21,7 +21,7 @@ const Lanading = ({ }) => {
         <SectionAimedAt />
       </Box>
       <Box className="Landing__Section__Container Section_Hook">
-
+        <SectionHook />
       </Box>
     </Box>
   );
